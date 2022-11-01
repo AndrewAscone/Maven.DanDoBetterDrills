9be2c49dee8a08574bff05eedfe3418447204d28
AndrewAscone <andrew.ascone@gmail.com>
2022-11-01 10:06:42 -0400

    Completed string utilities tests

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
@@ -77,10 +77,8 @@ public class StringUtilities {
      * @return the first sequence of characters
      */
     public static String getFirstWord(String spaceDelimitedString){
-        String input = spaceDelimitedString;
-        String[] splits = input.split(" ");
-
-        return null;
+        String splitWord = spaceDelimitedString.split(" ", 2)[0];
+        return splitWord;
     }
 
     /**
@@ -88,7 +86,8 @@ public class StringUtilities {
      * @return the second word of a string delimited by spaces.
      */
     public static String getSecondWord(String spaceDelimitedString){
-        return null;
+        String splitWord = spaceDelimitedString.split(" ",2)[1];
+        return splitWord;
     }
 
     /**
